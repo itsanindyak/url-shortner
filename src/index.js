f8bf993a6cfb5864247eb0a53700d2fa3f5f38e8
itsanindyak <iankoley04@gmail.com>
2024-11-30 12:59:06 +0530

    Add Redis integration and update environment configuration

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import { app } from "./app.js";
-import connectDB from './db/index.js';
+import { connectDB, connectRedis } from "./db/index.js";
 
 dotenv.config({
-  path:"./.env"
-})
-
+  path: "./.env",
+});
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8400,()=>{
+  .then(() => {
+    app.listen(process.env.PORT || 8400, () => {
       console.log(`Server is running at port${process.env.PORT}`);
-      
-    })
-})
-.catch((error)=>{
-  console.log("MongoDB connection failed!!",error);
-  
-})
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed!!", error);
+  });
 
-
+export const client = await connectRedis();
