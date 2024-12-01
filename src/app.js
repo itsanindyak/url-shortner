e7a7fa380a84788c7fed7ac101cad68468a21896
itsanindyak <iankoley04@gmail.com>
2024-12-02 00:28:30 +0530

    Refactor SIGINT handling to perform cleanup and save Redis data to the database

--- src/app.js
+++ src/app.js
@@ -7,10 +7,7 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 job.start();
 
-process.on("SIGINT", redisToDb);
-
 import urlRoute from "../src/router/url.routers.js";
-import { redisToDb } from "./jobs/cleanup.js";
 
 app.use("/api/v1/url", urlRoute);
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
 import { connectDB, connectRedis } from "./db/index.js";
+import { redisToDb } from "./jobs/cleanup.js";
 
 dotenv.config({
   path: "./.env",
 });
+let server;
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8400, () => {
+    server = app.listen(process.env.PORT || 8400, () => {
       console.log(`Server is running at port${process.env.PORT}`);
     });
   })
   .catch((error) => {
     console.log("MongoDB connection failed!!", error);
   });
+
+process.on("SIGINT", async () => {
+  console.log("SIGINT received: Starting cleanup...");
+  try {
+    server.close(() => {
+      console.log("Server closed.");
+    });
+
+    // Perform Redis to DB operation
+    await redisToDb();
+    console.log("Redis data saved to database successfully.");
+  } catch (error) {
+    console.error("Error during cleanup:", error);
+  } finally {
+    process.exit(0);
+  }
+});
 
 export const client = await connectRedis();
