a5e71f6d55d9ca4a223de0a11dc98e5bdddcf92a
itsanindyak <iankoley04@gmail.com>
2024-12-01 21:20:08 +0530

    Initialize job scheduling and handle SIGINT for redisToDb function

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from "express";
 import cors from "cors";
+import { job } from "./jobs/index.js";
 const app = express();
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
+job.start();
+
+process.on("SIGINT", redisToDb);
 
 import urlRoute from "../src/router/url.routers.js";
+import { redisToDb } from "./jobs/cleanup.js";
 
 app.use("/api/v1/url", urlRoute);
 
